57afa53dd8edd570cc098c77b2df82dea096c458
Thomas G <github@t-jay.net>
2017-06-02 18:08:24 +0200

    split funcs into seperat peaces

--- DICOM_OpenCV/main.cpp
+++ DICOM_OpenCV/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/core.hpp>
+#include <opencv2\opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -13,65 +14,143 @@ static void help(char* progName)
 		<< progName << " [image_name -- default Assets/0001.jpg] " << std::endl << std::endl;
 }
 
+void takeDFT(cv::Mat& source, cv::Mat& target) 
+{
+	cv::Mat originalComplex[2] = { source, cv::Mat::zeros(source.size(), CV_32F) };
+
+	cv::Mat dftReady;
+
+	cv::merge(originalComplex, 2, dftReady);
+
+	cv::Mat dftOfOriginal;
+
+	cv::dft(dftReady, dftOfOriginal, cv::DFT_COMPLEX_OUTPUT);
+
+	target = dftOfOriginal;
+}
+
+void recenterDFT(cv::Mat& source) 
+{
+	int centerX = source.cols / 2;
+	int centerY = source.rows / 2;
+
+	cv::Mat q1(source, cv::Rect(0, 0, centerX, centerY));
+	cv::Mat q2(source, cv::Rect(centerX, 0, centerX, centerY));
+	cv::Mat q3(source, cv::Rect(0, centerY, centerX, centerY));
+	cv::Mat q4(source, cv::Rect(centerX, centerY, centerX, centerY));
+
+	cv::Mat swapMap;
+
+	q1.copyTo(swapMap);
+	q4.copyTo(q1);
+	swapMap.copyTo(q4);
+
+	q2.copyTo(swapMap);
+	q3.copyTo(q2);
+	swapMap.copyTo(q3);
+}
+
+void showDFT(cv::Mat& source) 
+{
+	cv::Mat splitArray[2] = {cv::Mat::zeros(source.size(), CV_32F), cv::Mat::zeros(source.size(), CV_32F) };
+
+	cv::split(source, splitArray);
+
+	cv::Mat dftMagnitude;
+
+	cv::magnitude(splitArray[0], splitArray[1], dftMagnitude);
+
+	dftMagnitude += cv::Scalar::all(1);
+
+	cv::log(dftMagnitude, dftMagnitude);
+
+	cv::normalize(dftMagnitude, dftMagnitude, 0, 1, CV_MINMAX);
+
+	cv::imshow("DFT", dftMagnitude);
+
+	cv::waitKey();
+}
+
+void invertDFT(cv::Mat& source, cv::Mat& target) {
+
+}
+
 int main(int argc, char ** argv)
 {
-	help(argv[0]);
+	cv::Mat original = cv::imread("Assets/lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-	const char* filename = argc >= 2 ? argv[1] : "Assets/0001.jpg";
+	cv::Mat originalFloat;
 
-	cv::Mat I = cv::imread(filename, cv::IMREAD_GRAYSCALE);
-	if (I.empty()) { return -1; }
+	original.convertTo(originalFloat, CV_32FC1, 1.0 / 255.0);
 
-	cv::Mat padded;                            //expand input image to optimal size
-	int m = cv::getOptimalDFTSize(I.rows);
-	int n = cv::getOptimalDFTSize(I.cols); // on the border add zero values
-	cv::copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+	cv::Mat dftOfOriginal;
 
-	cv::Mat planes[] = { cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F) };
-	cv::Mat complexI;
-	cv::merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+	takeDFT(originalFloat, dftOfOriginal);
 
-	cv::dft(complexI, complexI);            // this way the result may fit in the source matrix
+	recenterDFT(dftOfOriginal);
 
-										// compute the magnitude and switch to logarithmic scale
-										// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-	cv::split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-	cv::magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-	cv::Mat magI = planes[0];
-
-	magI += cv::Scalar::all(1);                    // switch to logarithmic scale
-	cv::log(magI, magI);
-
-	// crop the spectrum, if it has an odd number of rows or columns
-	magI = magI(cv::Rect(0, 0, magI.cols & -2, magI.rows & -2));
-
-	// rearrange the quadrants of Fourier image  so that the origin is at the image center
-	int cx = magI.cols / 2;
-	int cy = magI.rows / 2;
-
-	cv::Mat q0(magI, cv::Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-	cv::Mat q1(magI, cv::Rect(cx, 0, cx, cy));  // Top-Right
-	cv::Mat q2(magI, cv::Rect(0, cy, cx, cy));  // Bottom-Left
-	cv::Mat q3(magI, cv::Rect(cx, cy, cx, cy)); // Bottom-Right
-
-	cv::Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-	q0.copyTo(tmp);
-	q3.copyTo(q0);
-	tmp.copyTo(q3);
-
-	q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-	q2.copyTo(q1);
-	tmp.copyTo(q2);
-
-	normalize(magI, magI, 0, 1, cv::NORM_MINMAX); // Transform the matrix with float values into a
-											  // viewable image form (float between values 0 and 1).
-
-	imshow("Input Image", I);    // Show the result
-	imshow("spectrum magnitude", magI);
-	cv::waitKey();
-
-	return 0;
+	showDFT(dftOfOriginal);
 }
+
+//int main(int argc, char ** argv)
+//{
+//	help(argv[0]);
+//
+//	const char* filename = argc >= 2 ? argv[1] : "Assets/imageTextN.png";
+//
+//	cv::Mat I = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+//	if (I.empty()) { return -1; }
+//
+//	cv::Mat padded;                            //expand input image to optimal size
+//	int m = cv::getOptimalDFTSize(I.rows);
+//	int n = cv::getOptimalDFTSize(I.cols); // on the border add zero values
+//	cv::copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+//
+//	cv::Mat planes[] = { cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F) };
+//	cv::Mat complexI;
+//	cv::merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+//
+//	cv::dft(complexI, complexI);            // this way the result may fit in the source matrix
+//
+//										// compute the magnitude and switch to logarithmic scale
+//										// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+//	cv::split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+//	cv::magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+//	cv::Mat magI = planes[0];
+//
+//	magI += cv::Scalar::all(1);                    // switch to logarithmic scale
+//	cv::log(magI, magI);
+//
+//	// crop the spectrum, if it has an odd number of rows or columns
+//	magI = magI(cv::Rect(0, 0, magI.cols & -2, magI.rows & -2));
+//
+//	// rearrange the quadrants of Fourier image  so that the origin is at the image center
+//	int cx = magI.cols / 2;
+//	int cy = magI.rows / 2;
+//
+//	cv::Mat q0(magI, cv::Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+//	cv::Mat q1(magI, cv::Rect(cx, 0, cx, cy));  // Top-Right
+//	cv::Mat q2(magI, cv::Rect(0, cy, cx, cy));  // Bottom-Left
+//	cv::Mat q3(magI, cv::Rect(cx, cy, cx, cy)); // Bottom-Right
+//
+//	cv::Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+//	q0.copyTo(tmp);
+//	q3.copyTo(q0);
+//	tmp.copyTo(q3);
+//
+//	q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+//	q2.copyTo(q1);
+//	tmp.copyTo(q2);
+//
+//	normalize(magI, magI, 0, 1, cv::NORM_MINMAX); // Transform the matrix with float values into a
+//											  // viewable image form (float between values 0 and 1).
+//
+//	imshow("Input Image", I);    // Show the result
+//	imshow("spectrum magnitude", magI);
+//	cv::waitKey();
+//
+//	return 0;
+//}
 
 //int main(int argc, char** argv)
 //{
