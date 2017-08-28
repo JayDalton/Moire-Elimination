55a300fb4e9da92990814542de7a7681760c7d3c
Thomas G <github@t-jay.net>
2017-08-29 01:55:24 +0200

    added scaffolding for viewmodel

---
+++ ChartInUWP/ChartLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace ChartInUWP
+{
+  public class ChartLoader
+  {
+    #region Fields
+
+    #endregion Fields
+
+    public ChartLoader()
+    {
+
+    }
+
+    #region Methods
+
+    public async Task LoadFileSelection()
+    {
+      var picker = new FileOpenPicker();
+      picker.ViewMode = PickerViewMode.List;
+      picker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+      picker.FileTypeFilter.Add("*");
+
+      StorageFile file = await picker.PickSingleFileAsync();
+      if (file != null)
+      {
+
+
+      }
+    }
+
+    #endregion Methods
+  }
+}
---
+++ ChartInUWP/ChartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+
+namespace ChartInUWP
+{
+  public class ChartViewModel : ViewModelBase
+  {
+    #region Fields
+
+    private bool progressing = false;
+    private ChartLoader chartLoader;
+    private ChartRenderer chartRenderer;
+
+    #endregion Fields
+
+    public ChartViewModel()
+    {
+      chartLoader = new ChartLoader();
+      chartRenderer = new ChartRenderer();
+    }
+
+    #region Properties
+
+    public bool Progressing
+    {
+      get { return progressing; }
+      set { SetProperty(ref progressing, value); }
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    public async Task LoadChartFileData()
+    {
+      await chartLoader.LoadFileSelection();
+    }
+
+    #endregion Methods
+  }
+}
