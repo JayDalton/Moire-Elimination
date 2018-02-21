760cc1b19b5b7870ff1712383956ef639229d161
Thomas Gerlach <t.gerlach@inno-focus.com>
2018-02-21 21:59:50 +0100

    added console application

--- ChartWRCLibrary/DicomService.hpp
+++ ChartWRCLibrary/DicomService.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-//using namespace Dicom;
-//using namespace Dicom::Imaging;
-//using namespace Dicom::Imaging::Render;
+
 
 class DicomService
 {
 private:
-	//DicomFile _dicomFile;
 
 public:
 	DicomService();
