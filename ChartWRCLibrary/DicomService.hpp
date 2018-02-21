bcccdcbb3f7f2fe0173cf832f37d42be000011a5
Thomas Gerlach <t.gerlach@inno-focus.com>
2018-02-21 18:22:36 +0100

    added native dll library

--- ChartWRCLibrary/DicomService.hpp
+++ ChartWRCLibrary/DicomService.hpp
@@ -1,6 +1,14 @@
 #pragma once
+
+//using namespace Dicom;
+//using namespace Dicom::Imaging;
+//using namespace Dicom::Imaging::Render;
+
 class DicomService
 {
+private:
+	//DicomFile _dicomFile;
+
 public:
 	DicomService();
 	~DicomService();
